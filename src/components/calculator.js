24d226e0e8080a2fed26decdc5c28a1ab133aa3c
Donmark2k <chukwuemekavp@gmail.com>
2023-02-16 07:56:59 +0100

    Restructuring the class component into funtional component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -14,6 +14,7 @@ const Calculator = () => {
   // Array destructuring
   const { total, operation, next } = state;
 
+  // This is the click function
   const onClickHandler = (event) => {
     const TargetValue = event.target.value;
     const updateState = calculate(state, TargetValue);
