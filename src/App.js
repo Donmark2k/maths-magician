c33a1b18585c4915cf9aa3af41daf96f6301cfd3
Donmark2k <chukwuemekavp@gmail.com>
2023-02-22 04:59:42 +0100

    import Routes and Route on app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Calculator from './components/calculator';
+import { Route, Routes} from 'react-router-dom';
+import NavBar from './components/navBar';
+import Quotes from './components/quote';
+import Home from './components/home';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+<>
+     <NavBar/>
+         <Routes>
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quotes" element={<Quotes />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+
+</>
     </div>
   );
 }
