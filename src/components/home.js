8a60297ce9492ecdd8337dcdc8ee86773cf24c77
Donmark2k <chukwuemekavp@gmail.com>
2023-02-24 06:09:05 +0100

    Explain the use of the site in the home page

--- src/components/home.js
+++ src/components/home.js
@@ -7,24 +7,24 @@ const Home = () => (
       <h2>Welcome to our page!</h2>
       <br />
       <p className="homeP">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      Maths Magician is a website that provides a fun and interactive way for 
+      children to practice and improve their math skills. The website offers a variety
+       of math games and activities designed to help students learn and understand basic 
+       math concepts such as addition, subtraction, multiplication, division, and more.
+      <br/>
+       The games on Maths Magician are designed to be both engaging and educational, 
+       providing students with immediate feedback on their progress and encouraging them 
+       to continue practicing their math skills. The website is designed for children in grades K-8 
+       and offers games and activities at a variety of difficulty levels, 
+       so children can progress at their own pace. The quotes can serve many purposes, 
+       such as inspiring and motivating students to learn math, 
+       conveying the beauty and elegance of mathematical concepts, or 
+       providing insight and wisdom about the role of math in our lives and the world around you.
       </p>
       <p className="homeP">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      Overall, Maths Magician is a great resource for parents and teachers who 
+      are looking for ways to help their children improve their math skills in 
+      a fun and interactive way.
       </p>
     </div>
   </section>
