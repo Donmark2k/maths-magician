202b2f59350d0fbb49e2e2756436649e905e5f8d
Donmark2k <chukwuemekavp@gmail.com>
2023-02-24 05:22:53 +0100

    Add the API

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,40 +1,41 @@
 import axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Quote = () => {
-const [quote, setQuote] = useState("");
-const [author , setAuthor] = useState ("");
-const quoteAPI = async () => {
-  let arrayQuotes = [] ;
-  try {
-    const data = await axios.get("https://api.quotable.io/random");
-    arrayQuotes = data.data;
-  } catch (error) {
-    console.log(error)
-  }
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const quoteAPI = async () => {
+    let arrayQuotes = [];
+    try {
+      const data = await axios.get('https://api.quotable.io/random');
+      arrayQuotes = data.data;
+    } catch (error) {
+      console.log(error);
+    }
 
-  try {
-    setQuote(arrayQuotes.content)
-    setAuthor(arrayQuotes.author)
-  }catch (error) {
-    console.log(error)
-  }
-} 
+    try {
+      setQuote(arrayQuotes.content);
+      setAuthor(arrayQuotes.author);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-useEffect(() => {
-  quoteAPI();
-  
-}, [])
+  useEffect(() => {
+    quoteAPI();
+  }, []);
 
-return ( 
-<>
-  <div className="quoteBox">{quote}</div>
-  <p className="quoteAuthor" >{author} </p>
-  
-  <button className="quoteBtn" onClick={quoteAPI}>GIMME QUOTES </button>
-  </>
-)
-  
+  return (
+    <>
+      <div className="quoteBox">{quote}</div>
+      <p className="quoteAuthor">
+        {author}
+        {' '}
+      </p>
+
+      <button className="quoteBtn" onClick={quoteAPI}>GIMME QUOTES </button>
+    </>
+  );
 };
 
 export default Quote;
